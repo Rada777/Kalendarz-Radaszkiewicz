c57a8d63bf4077eca65e81a341530b4f478fa4f4
Rada777 <96410194+Rada777@users.noreply.github.com>
2022-03-20 19:48:09 +0100

    Update Calendar.js

--- Calendar.js
+++ Calendar.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import BigCalendar from "react-big-calendar";
 import moment from "moment";
-
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import TextField from "material-ui/TextField";
+import TimePicker from "material-ui/TimePicker";
 require("react-big-calendar/lib/css/react-big-calendar.css");
 
 BigCalendar.momentLocalizer(moment);
@@ -35,6 +38,65 @@ class Calendar extends Component {
       end: slotInfo.end,
       openSlot: true
     });
+  }
+
+  handleEventSelected(event) {
+    console.log("event", event);
+    this.setState({
+      openEvent: true,
+      clickedEvent: event,
+      start: event.start,
+      end: event.end,
+      title: event.title,
+      desc: event.desc
+    });
+  }
+
+  setTitle(e) {
+    this.setState({ title: e });
+  }
+
+  setDescription(e) {
+    this.setState({ desc: e });
+  }
+
+  handleStartTime = (event, date) => {
+    this.setState({ start: date });
+  };
+
+  handleEndTime = (event, date) => {
+    this.setState({ end: date });
+  };
+
+  // funkcja Onclick - nowy event
+  setNewAppointment() {
+    const { start, end, title, desc } = this.state;
+    let appointment = { title, start, end, desc };
+    let events = this.state.events.slice();
+    events.push(appointment);
+    this.setState({ events });
+  }
+
+  //  Update'uje tytul lub opis eventu
+  updateEvent() {
+    const { title, desc, start, end, events, clickedEvent } = this.state;
+    const index = events.findIndex(event => event === clickedEvent);
+    const updatedEvent = events.slice();
+    updatedEvent[index].title = title;
+    updatedEvent[index].desc = desc;
+    updatedEvent[index].start = start;
+    updatedEvent[index].end = end;
+    this.setState({
+      events: updatedEvent
+    });
+  }
+
+  //  usuwa event
+  deleteEvent() {
+    let updatedEvents = this.state.events.filter(
+      event => event["start"] !== this.state.start
+    );
+    this.setState({ events: updatedEvents });
   }
 }
 
